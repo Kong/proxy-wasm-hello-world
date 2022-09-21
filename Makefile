c4bb60fb407c85eceaddbb6eed2b4bcc33484d67
usrbinkat <usrbinkat@braincraft.io>
2022-09-20 22:38:12 -0700

    simple add header

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 default:
+	rustup target add wasm32-unknown-unknown
 	cargo build --target=wasm32-unknown-unknown
+clean:
+	rm -rf target
--- src/filter.rs
+++ src/filter.rs
@@ -1,8 +1,5 @@
-mod soaptojson;
-
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-use soaptojson::{to_json, go_to_body};
 
 #[no_mangle]
 pub fn _start() {
@@ -31,17 +28,6 @@ impl Context for HttpBody {}
 impl HttpContext for HttpBody {
     fn on_http_response_headers(&mut self, _: usize) -> Action {
         self.set_http_response_header("content-length", None);
-        Action::Continue
-    }
-
-    fn on_http_response_body(&mut self, body_size: usize, _end_of_stream: bool) -> Action {
-        if let Some(body_bytes) = self.get_http_response_body(0, body_size) {
-            let body_str = String::from_utf8(body_bytes).unwrap();
-            let value = to_json(&body_str, false).unwrap();
-            let body = go_to_body(&value).unwrap();
-            let text = serde_json::to_string(&body).unwrap();
-            self.set_http_response_body(0, body_size, &text.into_bytes());
-        }
         Action::Continue
     }
 }
